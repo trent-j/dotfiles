85edb6173d885d311a57025b4147ec24c3270a32
Trent Jones <trent-j@github.com>
2020-07-18 13:27:35 -0500

    sudo it

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Allow root login
-sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-mkdir -p /root/.ssh && cp ~/.ssh/authorized_keys $_
-systemctl restart ssh
+sudo sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo mkdir -p /root/.ssh && sudo cp /workspace/.ssh/authorized_keys $_
+sudo systemctl restart ssh
