c1947c2ab306496052d668a7109af964ab1e1cc5
Trent Jones <trent-j@github.com>
2020-08-30 04:15:46 -0500

    accept gh_pat

--- enterprise/setup.sh
+++ enterprise/setup.sh
@@ -14,6 +14,7 @@ setup_git () {
 setup_ssh () {
     sudo sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
     echo 'AcceptEnv OCTOFACTORY_TOKEN' | sudo tee -a /etc/ssh/sshd_config
+    echo 'AcceptEnv GH_PAT' | sudo tee -a /etc/ssh/sshd_config
     sudo mkdir -p /root/.ssh && sudo cp "$DIR/ssh/config" "$_"
     sudo cat /workspace/.ssh/authorized_keys | sudo tee -a /root/.ssh/authorized_keys
     sudo systemctl restart ssh
