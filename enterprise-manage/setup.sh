04a3ec513a8df9777c2e8db1d169f4b478f9a899
Trent Jones <trent-j@github.com>
2020-09-27 04:38:21 -0500

    fix gem permissions

--- enterprise-manage/setup.sh
+++ enterprise-manage/setup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
+EM_DIR='/workspace/enterprise2/enterprise-manage'
 
 setup_bash () {
     cat "$DIR/bash/.bashrc" >> ~/.bashrc
 }
 
+setup_gem_permissions () {
+    chown -R "$USER" "$EM_DIR/vendor/gems/"
+}
+
 setup () {
     setup_bash
+    setup_gem_permissions
 }
 
 setup
