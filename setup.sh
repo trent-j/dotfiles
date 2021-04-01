87167a2979494ac79076deac1f759f39fb04e274
Trent Jones <trent-j@github.com>
2021-04-01 11:45:06 -0500

    add log for dotfiles

--- setup.sh
+++ setup.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-echo "running dotfiles" > /tmp/dotfiles-status
-
 # shellcheck disable=SC2011,SC2156
-find "$(cd "$(dirname "$0")" && pwd)" -mindepth 2 -name 'setup.sh' -exec bash -c "chmod +x {} && {}" \;
+find "$(cd "$(dirname "$0")" && pwd)" -mindepth 2 -name 'setup.sh' -exec bash -c "chmod +x {} && {} 2>&1 >> /tmp/dotfile-log" \;
