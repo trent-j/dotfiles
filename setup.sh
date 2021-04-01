5ff1b3c159e66c18c1dbf6e7c23c88d2c3151291
Trent Jones <trent-j@github.com>
2021-04-01 11:04:58 -0500

    debug message

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+echo "running dotfiles" > /tmp/dotfiles-status
+
 # shellcheck disable=SC2011,SC2156
 find "$(cd "$(dirname "$0")" && pwd)" -mindepth 2 -name 'setup.sh' -exec bash -c "chmod +x {} && {}" \;
