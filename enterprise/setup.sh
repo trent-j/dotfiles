f702d8f6d7e21614d61d8f1b3f661c980a43a58a
Trent Jones <trent-j@github.com>
2021-04-01 12:58:26 -0500

    debug

--- enterprise/setup.sh
+++ enterprise/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 setup_bash_profile () {
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 # shellcheck disable=SC2011,SC2156
 find "$(cd "$(dirname "$0")" && pwd)" -mindepth 2 -name 'setup.sh' -exec bash -c "chmod +x {} && {} 2>&1 >> /tmp/dotfile-log" \;
